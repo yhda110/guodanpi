cb64639f69ffec09be72ebdd8eec22a483fbb22c
xinan.liu <yhda66@126.com>
2019-09-25 12:01:57 +0800

    修复上传图片功能

--- mobile/src/page/Upload/index.vue
+++ mobile/src/page/Upload/index.vue
@@ -14,13 +14,16 @@
 				v-model="uploadData.desc"
 				label="描述"
 				type="textarea"
-				placeholder="请输入留言"
+				placeholder="请输入描述（可不填）"
 				rows="5"
 				autosize/>
 			<van-uploader 
 				:before-read="beforeRead" 
 				:after-read="afterRead"
-				v-model="fileList" 
+				@delete="fileDelete"
+				v-model="fileList"
+				:max-count="6"
+				:max-size="5000000"
 				multiple/>
 		</div>
 	</div>
@@ -32,10 +35,7 @@ import md5 from 'md5'
 export default {
 	data() {
 		return {
-			fileList: [{
-				url: 'https://img.yzcdn.cn/vant/cat.jpeg',
-				isImage: true
-			}],
+			fileList: [],
 			uploadData: {
 				title: '',
 				desc: '',
@@ -50,7 +50,6 @@ export default {
 	mounted() {
 		// Toast('xxxxxxxxxxxxxx')
 		this.setState({key:'indexHeaderSHow',  value:false})
-		// this.setState({key:'headerShow',  value:false})
 	},
 	methods: {
 		...mapActions(['setState']),
@@ -58,11 +57,11 @@ export default {
 			this.$router.go(-1)
 		},
 		onClickRight() {
-
+			this.submit()
 		},
 		// 返回布尔值
     async beforeRead(file) {
-			// let _this = this
+			let _this = this
 
       if (!(file.type === 'image/jpeg' || file.type === 'image/png')) {
 				this.$Toast.fail({
@@ -77,60 +76,42 @@ export default {
 				duration: 0
 			})
 			let result = await this.$api('get', 'api/image/QiniuGetToken', {})
-			var observer = {
-				error(err){
-					console.log(err)
-				},
-				complete(){
-					_this.$Toast.clear({
-						clearAll: true
-					})
-			
-				}
-			}
-			this.$qiniu.compressImage(file, this.options).then(data => {
-				let key = `${md5(new Date().getTime() + file.name)}.${file.type.split('/')[1]}`
-				var observable = this.$qiniu.upload(data.dist, key, result.data.data.token)
-				observable.subscribe(observer) // 上传开始
+			await new Promise((resolve, reject) => {
+				this.$qiniu.compressImage(file, this.options).then(data => {
+					let key = `${md5(new Date().getTime() + file.name)}.${file.type.split('/')[1]}`
+					var observable = this.$qiniu.upload(data.dist, key, result.data.data.token)
+					observable.subscribe({
+							error(err){
+								console.log(err)
+								reject(err)
+							},
+							complete(res){
+								_this.$Toast.clear({
+									clearAll: true
+								})
+								let url = `${result.data.data.domain}/${res.key}`
+								_this.uploadData.imgList.push(url)
+								resolve()
+							}
+						}) // 上传开始
+				})
 			})
-
 			return true
-    },
-    // 返回 Promise
-    // asyncBeforeRead(file) {
-    //   return new Promise((resolve, reject) => {
-    //     if (file.type !== 'image/jpeg') {
-		// 			reject()
-		// 			return false
-    //     }
-    //   })
-		// },
-		async afterRead() {
-			// let _this = this
-			// this.$Toast.loading({
-			// 	mask: true,
-			// 	message: '图片上传中...',
-			// 	duration: 0
-			// })
-
-			// let result = await this.$api('get', 'api/image/QiniuGetToken', {})
-			// let options = {}
-			// var observer = {
-			// 	error(err){
-			// 		console.log(err)
-			// 	},
-			// 	complete(res){
-			// 		_this.$Toast.clear({
-			// 			clearAll: true
-			// 		})
-			// 		_this.uploadData.imgList.push(`${result.data.data.domain}/${res.key}`)
-			// 	}
-			// }
-			// this.$qiniu.compressImage(file.file, options).then(data => {
-			// 	let key = `${md5(new Date().getTime() + file.file.name)}.${file.file.type.split('/')[1]}`
-			// 	var observable = this.$qiniu.upload(data.dist, key, result.data.data.token)
-			// 	observable.subscribe(observer) // 上传开始
-			// })
+		},
+		fileDelete(file) {
+			console.log(file)
+		},
+		afterRead() {},
+		// 上传发帖
+		submit() {
+			// 检测完整性
+			console.log(this.fileList)
+			// console.log(this.uploadData.desc)
+			console.log(this.uploadData.title)
+			if(!this.uploadData.title) {
+				this.$Toast.fail({message: '请填写标题'})
+				return false
+			}
 		}
 	}
 }
