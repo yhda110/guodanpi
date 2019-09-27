8d1ccd7dc22892340675c4a8ff48655e2b4f994f
xinan.liu <yhda66@126.com>
2019-09-27 15:04:46 +0800

    尝试解决代理问题

--- mobile/src/main.js
+++ mobile/src/main.js
@@ -18,6 +18,7 @@ Vue.prototype.$qs = qs
 Vue.prototype.$qiniu = qiniu
 Vue.prototype.$Toast = Toast
 
+Axios.defaults.baseURL = '/api/'
 Vue.prototype.$api = function (type, url, data) {
   return new Promise((resolve, reject) => {
     Axios[type](url,data).then(data => {
--- mobile/vue.config.js
+++ mobile/vue.config.js
@@ -14,11 +14,11 @@ module.exports = {
 							'^/tencent': '/'  // 替换target中的请求地址，也就是说，在请求的时候，url用'/proxy'代替'http://ip.taobao.com'
 					}
 			},
-			'/': {
+			'/api': {
 				target: 'https://www.lzjrys.store/',   // 需要请求的地址
 				changOrigin: true,  // 是否跨域
 				pathRewrite: {
-						'^/': '/'  // 替换target中的请求地址，也就是说，在请求的时候，url用'/proxy'代替'http://ip.taobao.com'
+						'^/api': '/'  // 替换target中的请求地址，也就是说，在请求的时候，url用'/proxy'代替'http://ip.taobao.com'
 				}
 			}
 		}  // 配置多个代理
