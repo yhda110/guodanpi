081126e63275d03dfa63f3ebb40a1b45e64d1e1f
xinan.liu <yhda66@126.com>
2019-09-29 18:30:02 +0800

    修复图标

--- crm/src/main.js
+++ crm/src/main.js
@@ -3,6 +3,7 @@ import './plugins/axios'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
 
---
+++ crm/src/views/MainView.vue
@@ -0,0 +1,3 @@
+<template>
+	<div>wwwwwwwwwww</div>
+</template>
