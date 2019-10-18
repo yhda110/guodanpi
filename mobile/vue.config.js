48ba05a7bc7fb9183119e9c39050fcbfbdfcfb4e
xinan.liu <yhda66@126.com>
2019-10-18 17:46:55 +0800

    no message

--- mobile/vue.config.js
+++ mobile/vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
 		// proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
 		proxy: {
 			'/api': {
-				target: process.env.NODE_ENV === 'production'?'https://www.lzjrys.store/api/':'http://test.lzjrys.store/api/',   // 需要请求的地址
+				target: process.env.NODE_ENV === 'production'?'https://www.lzjrys.store/api/':'https://test.lzjrys.store/api/',   // 需要请求的地址
 				changOrigin: true,  // 是否跨域
 				pathRewrite: {
 						'^/api': '/'  // 替换target中的请求地址，也就是说，在请求的时候，url用'/proxy'代替'http://ip.taobao.com'
