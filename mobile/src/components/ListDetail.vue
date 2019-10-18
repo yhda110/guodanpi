4ac74ca5beb1bef4cec3f089fafcbb4255006496
xinan.liu <yhda66@126.com>
2019-10-18 14:44:01 +0800

    no message

--- mobile/src/components/ListDetail.vue
+++ mobile/src/components/ListDetail.vue
@@ -1,25 +1,32 @@
 <template>
-	<van-image-preview
-		v-model="show"
-		:images="detailData.img_list"
-		:z-index="9999"
-		@change="onChange"
-	>
-		<template v-slot:index>
-			<p>{{detailData.title}}</p>
-			<p>{{ index+1 }}/{{detailData.img_list.length}}</p>
-		</template>
-		
-	</van-image-preview>
+	<div class="main">
+		<van-image-preview
+			v-model="show"
+			:images="detailData.img_list"
+			:z-index="9999"
+			@change="onChange"
+		>
+			<template v-slot:index>
+				<p>{{detailData.title}}</p>
+				<p>{{ index+1 }}/{{detailData.img_list.length}}</p>
+			</template>
+		</van-image-preview>
+		<div class="bottomCard" v-show="show">
+			我这里是卡片位置
+		</div>
+	</div>
 </template>
 <script>
 export default {
 	data() {
 		return ({
-			show: false,
+			show: true,
 			index: 0,
 			detailData: {
-				img_list: []
+				img_list: ["https://lzjrys.store/8b81247f5954467bea17b420c7378aed.jpeg","https://lzjrys.store/67e5022ed45ca6e331dea37e742dfc20.png"],
+				title: '测试',
+				content: '我是测试内容我是测试内容我是测试内容我是测试内容我是测试内容我是测试内容我是测试内容我是测试内容我是测试内容',
+				create_time: '2019-09-26 09:58:03'
 			}
 		})
 	},
@@ -39,7 +46,15 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-	p{
-		text-align: center;
+	.main{
+		.bottomCard{
+			background: #fff;
+			position: fixed;
+			height: 1.333333rem;
+			width: 100vw;
+			bottom: 0;
+			left: 0;
+			z-index: 99999;
+		}
 	}
 </style>
