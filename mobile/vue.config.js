9d1aa0b3724af8e3f82e1060f47782473df366a4
xinan.liu <yhda66@126.com>
2019-10-18 15:17:55 +0800

    兼容生产环境路径

--- mobile/vue.config.js
+++ mobile/vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+	publicPath: process.env.NODE_ENV === 'production' ? '{$__PUBLIC__}' : './',
   devServer: {
 		port: 8080, // 端口号
 		host: '0.0.0.0',
@@ -7,15 +8,8 @@ module.exports = {
 		open: false, //配置自动启动浏览器
 		// proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
 		proxy: {
-			'/tencent': {
-					target: 'https://pacaio.match.qq.com',   // 需要请求的地址
-					changOrigin: true,  // 是否跨域
-					pathRewrite: {
-							'^/tencent': '/'  // 替换target中的请求地址，也就是说，在请求的时候，url用'/proxy'代替'http://ip.taobao.com'
-					}
-			},
 			'/api': {
-				target: 'http://test.lzjrys.store/api/',   // 需要请求的地址
+				target: process.env.NODE_ENV === 'production'?'https://www.lzjrys.store/api/':'http://test.lzjrys.store/api/',   // 需要请求的地址
 				changOrigin: true,  // 是否跨域
 				pathRewrite: {
 						'^/api': '/'  // 替换target中的请求地址，也就是说，在请求的时候，url用'/proxy'代替'http://ip.taobao.com'
