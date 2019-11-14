a7838c82cf7f5f43aebe05feb806dea123189c29
wsq95 <153646266@qq.com>
2019-11-14 10:49:36 +0800

    修改图片上传

--- crm/src/plugins/element.js
+++ crm/src/plugins/element.js
@@ -1,12 +1,13 @@
-import {Message, Tag, Option, OptionGroup, Input, Button} from 'element-ui'
+import { Message, Tag, Progress, Option, OptionGroup, Input, Button } from 'element-ui'
 const element = {
   install: function (Vue) {
-	Vue.use(Tag)
-    Vue.use(Option)
-    Vue.use(OptionGroup)
-    Vue.use(Input)
-	Vue.use(Button)
-	Vue.prototype.$message = Message
+  Vue.use(Tag);
+  Vue.use(Option);
+  Vue.use(Progress);
+  Vue.use(OptionGroup);
+  Vue.use(Input);
+  Vue.use(Button);
+  Vue.prototype.$message = Message;
   }
 }
 export default element
--- crm/src/views/upload.vue
+++ crm/src/views/upload.vue
@@ -7,14 +7,28 @@
       <v-row align="center" justify="center">
         <v-col cols="16" sm="12" md="8">
           <v-card class="mx-auto" style="display:flex;flex-wrap:wrap;">
-            <div style="padding: 10px 10px 0 0; box-sizing:border-box;">
-              <input class="upload" @change="change" type="file" multiple accept="image/*" />
+            <div>
+              <div class="imgTop">
+                <div class="imgbtn" >
+                  <div class="uploadTop">
+                    <img src="../assets/defalut.png" alt />
+                    <span>选取文件</span>
+                  </div>
+                  <input class="upload" @change="change" type="file" multiple accept="image/*" />
+                </div>
+                <v-btn color="primary" :x-small="true" @click="submit">上传照片</v-btn>
+              </div>
               <ul>
-                <li v-for="(item,index) in newImage" :key="index">
-                  <img :src="item" alt />
+                <li style="margin-bottom:10px;" v-for="(item,index) in newImage" :key="index">
+                  <div class="imgBox">
+                    <div class="btns" v-clipboard:copy="item.img" v-clipboard:success="onCopy" v-clipboard:error="onError" v-if="item.isshow&&item.status=='success'">复制</div>
+                    <img :src="item.img" alt />
+                    <div class="progress" v-if="item.isshow">
+                      <el-progress type="circle"  :status="item.status" :percentage="item.progress"></el-progress>
+                    </div>
+                  </div>
                 </li>
               </ul>
-              <v-btn color="primary" :x-small="true" @click="submit">上传</v-btn>
             </div>
             <!-- <v-card-actions>
             <div class="flex-grow-1"></div>-->
@@ -37,6 +51,12 @@ export default {
     newImage: []
   }),
   methods: {
+    onCopy(e){
+     alert('复制成功！')
+    },
+    onError(e){
+      alert('抱歉，该浏览器不支持复制！')
+    },
     change(e) {
       let files = e.target.files;
       console.log(files);
@@ -60,36 +80,138 @@ export default {
         // }
         reader.readAsDataURL(file);
         reader.onload = e => {
-          console.log("file 转 base64结果：" + e.target.result);
-          this.newImage.push(e.target.result);
+          let obj = {
+            img: e.target.result,
+            progress: 0,
+            status: "",
+            isshow:false
+          };
+          this.newImage.push(obj);
         };
       }
       console.log(this.newImage);
     },
-    submit(){
-      if(this.newImage.length>0){
-        var arr=[]
-        for(let i=0;i<this.newImage.length;i++){
-          if(i>=this.newImage.length){
-               if(arr.includes(2)){
-                 alert('上传失败')
-               }else{
-                 alert('上传成功')
-               }
-          }else{
-            this.$axios.post("/api/image/postImage", {img:this.newImage[i]}).then(res=>{
-                if(res.data.flag==true){
-                    arr.push(1)
-                }else{
-                  arr.push(2)
-                }
-            })
-          }
+    async submit() {
+      // var arr = [1, 2, 3, 4, 5];
+      // const getnum = num => {
+      //   return new Promise(resolve => setTimeout(resolve(num), 5000));
+      // };
+      // const forloop = async () => {
+      //   console.log("start");
+      //   for (let i = 0; i < arr.length; i++) {
+      //     const num = await getnum(arr[i]);
+      //     console.log(num);
+      //   }
+      //   console.log("end");
+      // };
+      // forloop();
+      // this.newImage=this.newImage.reverse()
+      if (this.newImage.length > 0) {
+        for (let i = 0; i < this.newImage.length; i++) {
+
+            await this.uploadimg(this.newImage[i].img,i)
         }
       }
+    },
+   uploadimg(val,index) {
+     this.newImage[index].isshow=true
+      this.$axios.post("/api/image/postImage", { img: val })
+        .then(res => {
+          if (res.data.flag == true) {
+            this.newImage[index].progress=100;
+             this.newImage[index].status='success'
+             this.newImage[index].img=res.data.data.url
+          } else {
+            this.newImage[index].status='exception'
+          }
+        });
+      // this.newImage[index].isshow=false
     }
   }
 };
 </script>
 <style lang="scss">
+.imgTop {
+  display: flex;
+  align-items: center;
+  .imgbtn {
+    width: 200px;
+    height: 30px;
+    position: relative;
+    margin-right: 20px;
+    .upload {
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 1;
+      display: block;
+      width: 200px;
+      height: 30px;
+      opacity: 0;
+    }
+    .uploadTop {
+      width: 200px;
+      height: 30px;
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: 0;
+      display: flex;
+      align-items: center;
+      // background:#fff;
+      img {
+        width: 16px;
+        height: 16px;
+        margin: 0 10px;
+      }
+      span {
+        color: #999;
+        font-size: 16px;
+      }
+    }
+  }
+}
+
+.imgBox {
+  width: 200px;
+  height: 200px;
+  position: relative;
+  .btns{
+    width: 60px;
+    height: 20px;
+    border-radius: 20px;
+    text-align: center;
+    line-height: 20px;
+    background: #2196F3;
+    font-size: 12px;
+    color:#fff;
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    z-index: 3;
+    cursor: pointer;
+  }
+  img {
+    width: 200px;
+    height: 200px;
+    position: absolute;
+    border-radius: 8px;
+    top: 0;
+    left: 0;
+    z-index: 0;
+  }
+  .progress {
+    position: absolute;
+    border-radius: 8px;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    background: rgba(0, 0, 0, 0.2);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
 </style>
