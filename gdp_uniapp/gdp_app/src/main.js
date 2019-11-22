c78ea66dd8a661f966bbea6cf61e1272047ae998
ansheng1021 <ansheng1021@163.com>
2019-11-22 17:50:09 +0800

    添加登录页面、修改manifest.json、请求配置等

--- gdp_uniapp/gdp_app/src/main.js
+++ gdp_uniapp/gdp_app/src/main.js
@@ -4,7 +4,7 @@ import App from './App'
 Vue.config.productionTip = false
 
 Vue.prototype.$host = 'https://unidemo.dcloud.net.cn/';
-
+Vue.prototype.$base_url = 'http://test.lzjrys.store/';
 App.mpType = 'app'
 
 const app = new Vue({
