79ce4f9cf0d42179a394edf0eb816039e407680f
ansheng1021 <ansheng1021@163.com>
2019-11-29 09:30:00 +0800

    修改请求地址

--- gdp_uniapp/gdp_app/src/main.js
+++ gdp_uniapp/gdp_app/src/main.js
@@ -5,7 +5,7 @@ Vue.config.productionTip = false
 
 Vue.prototype.$host = 'https://unidemo.dcloud.net.cn/';
 // Vue.prototype.$base_url = 'http://test.lzjrys.store/'; //测试
-Vue.prototype.$base_url = 'http://www.lzjrys.store/'; //线上
+Vue.prototype.$base_url = 'https://www.lzjrys.store/'; //线上
 App.mpType = 'app'
 
 const app = new Vue({
